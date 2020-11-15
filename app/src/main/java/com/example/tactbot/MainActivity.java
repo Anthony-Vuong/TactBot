8db4d9e103c8c73ac290f1d268f5a8dbf9d724db
Anthony-Vuong <anthony.l.vuong@gmail.com>
2020-11-15 11:33:41 -0800

    Bluetooth Enable

--- app/src/main/java/com/example/tactbot/MainActivity.java
+++ app/src/main/java/com/example/tactbot/MainActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.bluetooth.BluetoothAdapter;
 import android.content.Intent;
+import android.content.IntentFilter;
+import android.util.Log;
 import android.view.View;
 import android.os.Bundle;
 import android.widget.Switch;
@@ -12,34 +14,51 @@ import android.widget.Toast;
 public class MainActivity extends AppCompatActivity {
 
     BluetoothAdapter bluetoothAdapter;
-    Switch sw1;
+
+    // Create a BroadcastReceiver for ACTION_FOUND.
+    private final broadCastReceiver receiver = new BroadcastReceiver() {
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+            if (action.equals(bluetoothAdapter.ACTION_STATE_CHANGED)) {
+
+            }
+        }
+    };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Switch ctrlBLE = (Switch)findViewById(R.id.enSwitch);
+
         bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
-        sw1 = (Switch)findViewById(R.id.enSwitch);
+
+        ctrlBLE.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                controlBluetooth();
+            }
+        });
     }
 
-    public void enableBluetooth(View v)
+    public void controlBluetooth()
     {
-        if (sw1.isChecked()) {
-            if(bluetoothAdapter == null){
-                Toast.makeText(getApplicationContext(), "Bluetooth is not supported.", Toast.LENGTH_SHORT).show();
-            }
-            else{
-                if(!bluetoothAdapter.isEnabled()){
-                    Intent i = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
-                    startActivityForResult(i, 1);
-                }
-            }
+        if(bluetoothAdapter == null){
+            Log.d(TAG, "Bluetooth is not available. ");
         }
-        else {
-            if(!bluetoothAdapter.isEnabled()){ }
-            else{
-                bluetoothAdapter.disable();
-            }
+        else if(bluetoothAdapter.isEnabled()){
+            bluetoothAdapter.disable();
+            IntentFilter intentBLE = new IntentFilter(BluetoothAdapter.ACTION_STATE_CHANGED);
+            registerReceiver(broadCastReceiver, intentBLE);
         }
+        else{
+            Intent intentEnableBLE = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivity(intentEnableBLE);
+
+            IntentFilter intentBLE = new IntentFilter(BluetoothAdapter.ACTION_STATE_CHANGED);
+            registerReceiver(broadCastReceiver, intentBLE);
+        }
+
     }
 
 
