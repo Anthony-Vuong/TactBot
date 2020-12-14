c748323c00f2b755fbe292040053d53062147d16
Anthony-Vuong <anthony.l.vuong@gmail.com>
2020-12-14 09:20:37 -0800

    Turret python file
    New turret control class reated using motor class and k theroem. Automotation concept needed

---
+++ TurretFeature/turret.py
@@ -0,0 +1,22 @@
+import pyb
+from motor import motor
+
+
+
+class Turret:
+    
+    
+    def __init__(self, motor1, motor2):
+        self.horizontalMotor = motor1
+        self.verticalMotor = motor2
+        
+        
+    def locate(self):
+        #not sure how to do this
+        
+    def rotateHorizontal(self, position):
+        #not sure how to do this
+        
+    def rotateVertical(self, position):
+        #not sure how to do this
+
